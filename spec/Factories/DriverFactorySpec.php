0d3eb725a3d20bf8088a06f1a5fbbd95148f8f87
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2018-05-19 19:01:36 +0100

    Factory can now resolve the FCM driver

--- spec/Factories/DriverFactorySpec.php
+++ spec/Factories/DriverFactorySpec.php
@@ -5,6 +5,8 @@ namespace spec\Matthewbdaly\PushNotification\Factories;
 use Matthewbdaly\PushNotification\Factories\DriverFactory;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
+use Http\Discovery\Strategy\MockClientStrategy;
+use Http\Discovery\HttpClientDiscovery;
 
 class DriverFactorySpec extends ObjectBehavior
 {
@@ -16,5 +18,14 @@ class DriverFactorySpec extends ObjectBehavior
     function it_implements_factory_contract()
     {
         $this->shouldImplement('Matthewbdaly\PushNotification\Contracts\Factory');
+    }
+
+    function it_returns_fcm_driver()
+    {
+        HttpClientDiscovery::prependStrategy(MockClientStrategy::class);
+        $args = [
+            'id' => 'cheesy-lentils-123456'
+        ];
+        $this->create($args)->shouldReturnAnInstanceOf('Matthewbdaly\PushNotification\Drivers\FCM');
     }
 }
--- src/Factories/DriverFactory.php
+++ src/Factories/DriverFactory.php
@@ -3,7 +3,18 @@
 namespace Matthewbdaly\PushNotification\Factories;
 
 use Matthewbdaly\PushNotification\Contracts\Factory;
+use Matthewbdaly\PushNotification\Drivers\FCM;
+use Http\Discovery\HttpClientDiscovery;
+use Http\Discovery\MessageFactoryDiscovery;
+use Http\Client\HttpClient;
+use Http\Message\MessageFactory;
 
 class DriverFactory implements Factory
 {
+    public static function create($args, HttpClient $client = null, MessageFactory $messageFactory = null)
+    {
+        $client = $client ?: HttpClientDiscovery::find();
+        $messageFactory = $messageFactory ?: MessageFactoryDiscovery::find();
+        return new FCM($client, $messageFactory);
+    }
 }
